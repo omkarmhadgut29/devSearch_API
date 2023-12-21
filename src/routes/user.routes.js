ae7fbe1a362cd522081911898926da465a1572d2
omkarmhadgut29 <omkarmhadgut29@gmail.com>
2023-12-21 18:37:46 +0530

    added route for refresh-tokens

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,16 @@
 import { Router } from "express";
-import { loginUser, registerUser } from "../controllers/user.controllers.js";
+import {
+  loginUser,
+  refreshAccessToken,
+  registerUser,
+} from "../controllers/user.controllers.js";
 
 const router = Router();
 
 router.route("/login").post(loginUser);
 
 router.route("/register").post(registerUser);
+
+router.route("/refresh-tokens").post(refreshAccessToken);
 
 export const userRouter = router;
